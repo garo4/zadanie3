ebb4e76f6660e0fa6a94d6d1ebad33d86b16f1d3
Nikita Sleptsov <nik.sleptsov56@mail.ru>
2020-11-23 18:47:29 +0900

    second commit

--- Zadanie3/MainWindow.xaml.cs
+++ Zadanie3/MainWindow.xaml.cs
@@ -15,14 +15,73 @@ using System.Windows.Shapes;
 
 namespace Zadanie3
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string a = numberA.Text, b = numberB.Text;
+            int A = Convert.ToInt32(a), B = Convert.ToInt32(b), d = b.Length - 1;
+            char[] c = b.ToCharArray();
+
+            for (; B > 0; d--)
+            {
+                if (c[c.Length - 1] == '3' || c[c.Length - 1] == '5' || c[c.Length - 1] == '7' || c[c.Length - 1] == '9')
+                {
+
+                    if (B == A)
+                    { 
+                        break;
+                    }
+                    if (B < A)
+                    {
+                        awnser.Text += ("Невозможно" + "\n");
+                        break;
+                    }
+
+                }
+                else if (c[c.Length - 1] == '1')
+                {
+                    B = (B - 1) / 10;
+                    awnser.Text += (B + "   Добавляем 1" + "\n");
+
+
+                    if (B == A)
+                    {   
+                        break;
+                    }
+                    if (B < A)
+                    {
+                        awnser.Text += ("Невозможно" + "\n");
+                        break;
+                    }
+                }
+
+                else if (c[c.Length - 1] == '2' || c[c.Length - 1] == '4' || c[c.Length - 1] == '6' || c[c.Length - 1] == '8' || c[c.Length - 1] == '0')
+                {
+                    B = B / 2;
+                    awnser.Text += (B + "   Умножаем на 2" + "\n");
+
+                    if (B == A)
+                    {     
+                        break;
+                    }
+
+                    if (B < A)
+                    {
+                        awnser.Text += ("Невозможно" + "\n");
+                        break;
+                    }
+                }
+
+                b = Convert.ToString(B);
+                c = b.ToCharArray();
+            }
         }
     }
 }
